4ea66db8be17ca95aed12301eacd8063bd09e845
Priscilla Wong <priscilla.wong6512@gmail.com>
2024-11-09 13:58:12 -0500

    Added login tsx and logo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,15 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from "./pages/Home.tsx";
+import Login from "./pages/Login.tsx";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
