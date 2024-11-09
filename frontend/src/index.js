3d4c6a7e8dc0d82bf4be6796d87e5c3944ea8082
Justin Zhang <justinzwork23@gmail.com>
2024-11-09 11:49:13 -0500

    front end init

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import Home from "./pages/Home.tsx";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -13,13 +12,12 @@ const router = createBrowserRouter([
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider>
+  <React.StrictMode>
     <RouterProvider router={router} />
-  </Provider>,
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <h1>HACKRPI 2024</h1>
+    </>
+  );
+}
+
+export default Home;
