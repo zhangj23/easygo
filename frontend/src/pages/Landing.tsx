42018e5fb51876b9e1197f7c156b7ff44c4f99b2
courteneyS <courteneycsagan@gmail.com>
2024-11-09 14:12:08 -0500

    Fixing image crop

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -4,7 +4,6 @@ function Landing() {
   return (
     <>
       <div className="h-screen bg-gradient-to-t from-blue-300 to-white-400">
-        <h1 className="text-5xl">Landing 2024</h1>
       </div>
     </>
   );
