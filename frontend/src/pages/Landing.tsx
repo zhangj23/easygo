637682328d03b38aabf0cc1a3ff145dd09a0e400
courteneyS <courteneycsagan@gmail.com>
2024-11-10 04:02:42 -0500

    Added text on landing page

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -15,7 +15,7 @@ function Landing() {
         <div className="flex-col">
           <h1 className="text-5xl font-bold">Built for Everyone,</h1>
           <h1 className="text-5xl font-bold py-2">Guided by You</h1>
-          <p className="text-lg py-4 w-3/4">Lorem ipsum odor amet, consectetuer adipiscing elit. Fringilla pretium torquent, consectetur fringilla sem lobortis. Sed nascetur tincidunt potenti facilisis molestie?</p>
+          <p className="text-lg py-4 w-3/4">Find easy-to-navigate wheelchair accessible routes throughout your city. Our route planner highlights barrier-free paths, ramps, and smooth surfaces to help you travel confidently and independently.</p>
         </div>
         <img className="flex" src="woman.jpg" alt="woman in wheelchair" />
       </div>
@@ -23,15 +23,15 @@ function Landing() {
       <div className="flex justify-evenly m-auto py-14 *:w-[26vw] *:h-[28vw] *:bg-white *:shadow-xl *:rounded-lg">
         <div>
           <h1 className="flex justify-center font-semibold text-4xl pt-6 pb-4">Our Mission</h1>
-          <p className="flex justify-center w-80 m-auto">Lorem ipsum odor amet, consectetuer adipiscing elit. Fringilla pretium torquent, consectetur fringilla sem lobortis. Sed nascetur tincidunt potenti facilisis molestie? Lobortis purus laoreet molestie est neque urna dignissim. Varius erat lobortis elit integer sodales metus. Neque aenean netus efficitur mattis potenti inceptos. Sodales placerat ex facilisis augue venenatis elementum!</p>
+          <p className="flex justify-center w-80 m-auto text-lg">Our mission is to make navigation easier and safer for everyone, especially those with accessibility needs. We aim to provide accessible routes, minimizing obstacles and enhancing independence for individuals with mobility challenges.</p>
         </div>
         <div>
           <h1 className="flex justify-center font-semibold text-4xl pt-6 pb-4">How Does It Work?</h1>
-          <p className="flex justify-center w-80 m-auto">Lorem ipsum odor amet, consectetuer adipiscing elit. Fringilla pretium torquent, consectetur fringilla sem lobortis. Sed nascetur tincidunt potenti facilisis molestie? Lobortis purus laoreet molestie est neque urna dignissim. Varius erat lobortis elit integer sodales metus. Neque aenean netus efficitur mattis potenti inceptos. Sodales placerat ex facilisis augue venenatis elementum!</p>
+          <p className="flex justify-center w-80 m-auto text-lg">Using data on path accessibility, this tool identifies routes that are wheelchair-friendly by avoiding stairs. Simply input your start and final destination, and our route finder will suggest the most accessible path for you.</p>
         </div>
         <div>
-          <h1 className="flex justify-center font-semibold text-4xl pt-6 pb-4">Our Team</h1>
-          <p className="flex justify-center w-80 m-auto">Lorem ipsum odor amet, consectetuer adipiscing elit. Fringilla pretium torquent, consectetur fringilla sem lobortis. Sed nascetur tincidunt potenti facilisis molestie? Lobortis purus laoreet molestie est neque urna dignissim. Varius erat lobortis elit integer sodales metus. Neque aenean netus efficitur mattis potenti inceptos. Sodales placerat ex facilisis augue venenatis elementum!</p>
+          <h1 className="flex justify-center font-semibold text-4xl pt-6 pb-4">Why it Matters</h1>
+          <p className="flex justify-center w-[40vh] m-auto text-lg">Over 1.3 billion people worldwide live with some form of disability, many of whom face daily challenges with mobility in public spaces. Accessible routes not only improve independence and safety but also foster a sense of belonging and inclusion.</p>
         </div>
       </div>
       
