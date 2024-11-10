ba0e987442d5d143c5bbd88f1d3283d25a69eb1c
Justin Zhang <justinzwork23@gmail.com>
2024-11-10 05:18:31 -0500

    finished the map

---
+++ frontend/src/components/SearchDropdown.tsx
@@ -0,0 +1,117 @@
+import React, { useState, useRef, useEffect } from "react";
+import { baseUrl } from "../config";
+import { FaSearchLocation } from "react-icons/fa";
+
+const SearchDropdown = (props) => {
+  const [searchTerm, setSearchTerm] = useState<String>("");
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
+  const [items, setItems] = useState({ hits: [] });
+  // const items = [
+  //   "Apple",
+  //   "Banana",
+  //   "Orange",
+  //   "Mango",
+  //   "Pineapple",
+  //   "Strawberry",
+  //   "Blueberry",
+  //   "Raspberry",
+  //   "Blackberry",
+  //   "Grape",
+  //   "Watermelon",
+  //   "Peach",
+  //   "Plum",
+  //   "Pear",
+  // ];
+
+  // const filteredItems = items.filter((item) =>
+  //   item.toLowerCase().includes(searchTerm.toLowerCase())
+  // );
+
+  const queryGet = async (e) => {
+    try {
+      await fetch(
+        `${baseUrl}/graph/api/geocoder?lat=${props.lat}&lon=${props.lon}&q=${e.target.value}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data.locations);
+          if (data.locations.hits === undefined) {
+            setIsOpen(false);
+          } else {
+            setItems(data.locations);
+            setIsOpen(true);
+          }
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+    setIsOpen(true);
+    queryGet(e);
+  };
+
+  const handleItemClick = (item, index) => {
+    if (index === 10) {
+      props.outLat(props.lat);
+      props.outLon(props.lon);
+    } else {
+      props.outLat(items.hits[index].point.lat);
+      props.outLon(items.hits[index].point.lng);
+    }
+
+    setSearchTerm(item);
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="" ref={dropdownRef}>
+      <div className="relative flex justify-between">
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={handleSearchChange}
+          onFocus={() => setIsOpen(true)}
+          placeholder={props.placeholder}
+          className="w-full outline-none bg-transparent text-black-600 text-sm"
+        />
+        <FaSearchLocation className="text-gray-500 cursor-pointer" />
+      </div>
+
+      {isOpen && Object.keys(items).length > 0 && (
+        <div className="absolute z-[10000] w-1/3 mt-1 bg-white border rounded-lg shadow-lg max-h-60 overflow-auto">
+          <div
+            onClick={() => handleItemClick("Current Location", 10)}
+            className="px-4 py-2 cursor-pointer hover:bg-gray-100"
+          >
+            Use Current Location
+          </div>
+          {items.hits.map((item, index) => (
+            <div
+              key={index}
+              onClick={() => handleItemClick(item.name, index)}
+              className="px-4 py-2 cursor-pointer hover:bg-gray-100"
+            >
+              {item.name}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SearchDropdown;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,58 +1,58 @@
 import React, { useEffect, useState } from "react";
 import MapComponent from "../components/MapComponent.tsx";
 import { baseUrl } from "../config";
-import polyline from "polyline";
 import { FaSearchLocation } from "react-icons/fa";
+import SearchDropdown from "../components/SearchDropdown.tsx";
 
 function Home() {
   const [currentLat, setLat] = useState<Number>();
   const [currentLong, setLong] = useState<Number>();
   const [route, setRoute] = useState();
-  const [key, setKey] = useState();
-  const getLocation = () => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
-      console.log(position);
-      fetchRoute(position);
-    });
-  };
+  const [startLat, setStartLat] = useState<Number>();
+  const [endLat, setEndLat] = useState<Number>();
+  const [startLon, setStartLon] = useState<Number>();
+  const [endLon, setEndLon] = useState<Number>();
 
-  const fetchRoute = async (position) => {
-    const start = {
-      lat: position.coords.latitude,
-      lon: position.coords.longitude,
-    }; // Berlin (example)
-    const end = { lat: 42.7308903, lon: -73.687679 }; // Another point in Berlin (example)
-
-    const url = `https://graphhopper.com/api/1/route?point=${start.lat},${start.lon}&point=${end.lat},${end.lon}&vehicle=foot&key=${key}`;
-
+  const fetchAccessible = async () => {
+    const end = { lat: 42.7308903, lon: -73.687679 };
     try {
-      const response = await fetch(url);
-      const data = await response.json();
-      console.log(data);
-      const encodedPoints = data.paths[0].points;
-      const decodedPoints = polyline
-        .decode(encodedPoints)
-        .map(([lat, lon]) => [lat, lon]);
-      setRoute(decodedPoints);
-      console.log(route);
+      fetch(`${baseUrl}/graph/api/route/wheelchair`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          startLat: startLat,
+          startLon: startLon,
+          endLat: endLat,
+          endLon: endLon,
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setRoute(data.route);
+          console.log(data.route);
+        });
     } catch (error) {
-      console.error("Error fetching route:", error);
+      console.log(error);
     }
   };
 
   useEffect(() => {
-    fetch(`${baseUrl}/graph/api/get-graph-key`)
-      .then((response) => response.json())
-      .then((data) => {
-        setKey(data.GRAPH_KEY);
-      })
-      .catch((error) => console.error("Error fetching the key:", error));
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
   }, []);
 
+  useEffect(() => {
+    if (startLat && endLat && startLon && endLon) {
+      fetchAccessible();
+      console.log("asd");
+    }
+  }, [startLat, endLat, startLon, endLon]);
   return (
-    <div className="h-[60vw] bg-gradient-to-t from-blue-300 to-white-400 font-poppins">
+    <div className="h-[200vh] bg-gradient-to-t from-blue-300 to-white-400 font-poppins">
       <div className="flex items-center justify-between px-10 py-2 bg-white shadow-lg">
         <a href="/home">
           <img className="w-2/3" src="logo.png" alt="logo" />
@@ -68,28 +68,31 @@ function Home() {
       </div>
 
       <div className="pt-8 pb-6">
-        <div className="flex space-x-4 justify-center *:flex *:rounded-full *:px-4 *:py-3 *:border-2 *:border-gray-400 *:max-w-lg *:overflow-hidden *:bg-white *:w-full *:sm:w-1/2">
-          <div>
-            <input
-              className="w-full outline-none bg-transparent text-black-600 text-sm"
-              type="email"
-              placeholder="From"
-            />
-            <FaSearchLocation className="text-gray-500" />
-          </div>
-          <div>
-            <input
-              className="w-full outline-none bg-transparent text-black-600 text-sm"
-              type="email"
-              placeholder="To"
-            />
-            <FaSearchLocation className="text-gray-500" />
-          </div>
+        <div className="flex space-x-4 justify-center *:rounded-full *:px-4 *:py-3 *:border-2 *:border-gray-400 *:max-w-lg *:overflow-hidden *:bg-white *:w-full *:sm:w-1/2">
+          <SearchDropdown
+            placeholder={"From"}
+            lat={currentLat}
+            lon={currentLong}
+            outLat={setStartLat}
+            outLon={setStartLon}
+          />
+
+          <SearchDropdown
+            placeholder={"To"}
+            lat={currentLat}
+            lon={currentLong}
+            outLat={setEndLat}
+            outLon={setEndLon}
+          />
         </div>
       </div>
 
-      <div className="py-2">
-        <div className="flex m-auto h-[40vw] w-[70vw] bg-white"></div>
+      <div className="py-2 w-2/3 m-auto">
+        {currentLat ? (
+          <MapComponent lat={currentLat} long={currentLong} route={route} />
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
